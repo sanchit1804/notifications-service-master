8161e8eed3215d728f2fd9cfeca873f52a189cb0
Sanchit Badyal <san@Sanchits-MacBook.local>
2026-06-19 15:08:19 +0530

    first commit

---
+++ src/main/java/com/project/dto/NotificationDTO.java
@@ -0,0 +1,37 @@
+package com.project.dto;
+
+public class NotificationDTO {
+
+	String message;
+
+	String notificationType;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the notificationType
+	 */
+	public String getnotificationType() {
+		return notificationType;
+	}
+
+	/**
+	 * @param notificationType the notificationType to set
+	 */
+	public void setnotificationType(String notificationType) {
+		this.notificationType = notificationType;
+	}
+
+}
